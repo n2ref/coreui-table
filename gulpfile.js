7b735580280aa0ca7ee3c26ffa606abb50cade10
shabuninil <shabuninil24@gmail.com>
2023-07-07 20:50:52 +0300

    В сборку добавлены не минифицированные файлы

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,8 @@ const rename     = require("gulp-rename");
 var conf = {
     dist: "./dist",
     js: {
-        file: 'coreui-table.min.js',
+        fileMin: 'coreui-table.min.js',
+        file: 'coreui-table.js',
         src: [
             'src/js/coreui.table.js',
             'src/js/coreui.table.ejs.js',
@@ -57,7 +58,8 @@ var conf = {
         ]
     },
     css: {
-        file: 'coreui-table.min.css',
+        fileMin: 'coreui-table.min.css',
+        file: 'coreui-table.css',
         src: [
             'src/css/coreui.table.css'
         ]
@@ -65,33 +67,49 @@ var conf = {
 };
 
 
-gulp.task('build_css', function(){
+gulp.task('build_css_min', function(){
     return gulp.src(conf.css.src)
         .pipe(sourcemaps.init())
         .pipe(cleanCSS())
+        .pipe(concat(conf.css.fileMin))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(conf.dist));
+});
+
+gulp.task('build_css_min_fast', function(){
+    return gulp.src(conf.css.src)
+        .pipe(sourcemaps.init())
+        .pipe(concat(conf.css.fileMin))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(conf.dist));
+});
+
+gulp.task('build_css', function(){
+    return gulp.src(conf.css.src)
+        .pipe(sourcemaps.init())
         .pipe(concat(conf.css.file))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(conf.dist));
 });
 
-gulp.task('build_css_fast', function(){
-    return gulp.src(conf.css.src)
+gulp.task('build_js_min', function() {
+    return gulp.src(conf.js.src)
         .pipe(sourcemaps.init())
-        .pipe(concat(conf.css.file))
+        .pipe(uglify())
+        .pipe(concat(conf.js.fileMin, {newLine: ";\n"}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(conf.dist));
+});
+
+gulp.task('build_js_min_fast', function() {
+    return gulp.src(conf.js.src)
+        .pipe(sourcemaps.init())
+        .pipe(concat(conf.js.fileMin, {newLine: ";\n"}))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(conf.dist));
 });
 
 gulp.task('build_js', function() {
-    return gulp.src(conf.js.src)
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(concat(conf.js.file, {newLine: ";\n"}))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(conf.dist));
-});
-
-gulp.task('build_js_fast', function() {
     return gulp.src(conf.js.src)
         .pipe(sourcemaps.init())
         .pipe(concat(conf.js.file, {newLine: ";\n"}))
@@ -133,8 +151,8 @@ gulp.task('build_tpl', function() {
 
 gulp.task('build_watch', function() {
     gulp.watch(conf.css.src, gulp.series(['build_css_fast']));
-    gulp.watch(conf.tpl.src, gulp.series(['build_tpl', 'build_js_fast']));
+    gulp.watch(conf.tpl.src, gulp.series(['build_tpl', 'build_js_min_fast']));
     gulp.watch(conf.js.src, gulp.parallel(['build_js_fast']));
 });
 
-gulp.task("default", gulp.series([ 'build_tpl', 'build_js', 'build_css' ]));
+gulp.task("default", gulp.series([ 'build_tpl', 'build_js_min', 'build_js', 'build_css_min', 'build_css' ]));
